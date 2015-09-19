6d2d3af94915a50e7f8c034b86860fdc652a1fda
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-19 20:04:14 +0400

    update script for import girls

--- scripts/import_vk_girls.py
+++ scripts/import_vk_girls.py
@@ -6,8 +6,9 @@ from app.db import db
 vk = vk_api.VkApi(os.environ['VK_LOGIN'], os.environ['VK_PASSWORD'])
 vk.authorization()
 
-# Сканируем все возраста от 14 до 21
-for i in range(int(input()), int(input())):
+girls = {}
+# Сканируем все возраста от 14 до 25
+for i in range(14, 26):
     response = vk.method('users.search', {
         'sort': 0,
         'count': 250,
@@ -20,12 +21,39 @@ for i in range(int(input()), int(input())):
     for girl in response['items']:
         if 'photo_200' not in girl:
             continue
-        db.girls.insert_one({
+        girls[girl['id']] = {
             'name': girl['first_name'] + ' ' + girl['last_name'],
             'photo': girl['photo_200'],
             'link': 'https://vk.com/id' + str(girl['id']),
             'rating': 1000,
             'checks': 0
-        })
-        print(girl['id'], '- done.')
+        }
+        print(girl['id'], '- added.')
     print(i, 'age done.')
+
+
+response = vk.method('groups.getMembers', {
+    'group_id': 'slyhivtk',
+    'count': 1000,
+    'fields': 'photo_200,sex',
+    'offset': 0
+})
+i = 0
+while response['items']:
+    for girl in response['items']:
+        # Если это действительно девушка и у нее есть фотка
+        if girl['sex'] == 1 and 'photo_200' in girl:
+            girls[girl['id']] = {
+                'name': girl['first_name'] + ' ' + girl['last_name'],
+                'photo': girl['photo_200'],
+                'link': 'https://vk.com/id' + str(girl['id']),
+                'rating': 1000,
+                'checks': 0
+            }
+
+    response = vk.method('groups.getMembers', {
+        'group_id': 'slyhivtk',
+        'count': 1000,
+        'fields': 'photo_200,sex',
+        'offset': i + 1000
+    })
