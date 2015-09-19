984a2b79dd3a29f424b2f37774f5910d846d5c3b
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-19 17:30:18 +0400

    fix bug with mongodb connection

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 import os
 from pymongo import MongoClient
 
-client = MongoClient(os.environ['MONGO_URL'])
+client = MongoClient(os.environ['MONGO_URL']).get_default_database()
 db = client.chicks_vtk
