7ce206bac855ba9603ae6d3ba3fc93355806ba09
Seva Zhidkov <zhidkovseva@gmail.com>
2015-09-20 20:20:32 +0400

    db fix

--- app/db.py
+++ app/db.py
@@ -1,5 +1,5 @@
 import os
 from pymongo import MongoClient
 
-client = MongoClient(os.environ['MONGO_URL']).get_default_database()
+client = MongoClient(os.environ['MONGO_URL'])
 db = client.chicks_vtk
